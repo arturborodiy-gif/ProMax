8cfbaa51f527d8919d4754c2f618ad7f2042af5f
arturborodiy-gif <arturborodiy@gmail.com>
2026-09-10 19:05:16 +0300

    Add files via upload

--- ProMax_MOBILE_CLOUD-sw.js
+++ ProMax_MOBILE_CLOUD-sw.js
@@ -1,7 +1,7 @@
-const CACHE='promax-mobile-cloud-v1';
-const ASSETS=['./ProMax_MOBILE_CLOUD_FINAL.html','./ProMax_MOBILE_CLOUD.webmanifest','./ProMax-icon-192.png','./ProMax-icon-512.png'];
+const CACHE='promax-mobile-cloud-v2';
+const ASSETS=['./index.html','./ProMax_MOBILE_CLOUD.webmanifest','./ProMax-icon-192.png','./ProMax-icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('promax-mobile-cloud-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
   if(u.origin===location.origin){
